85f4c25d0aa0971dcc9181c15a6901cb59a1688b
Matthew K. McMasters <matthewkmcmasters12@yahoo.com>
2016-07-09 14:57:59 -0400

    First commit from LT
    Working on moving the data into a separate javascript object.

--- js/coinring.js
+++ js/coinring.js
@@ -28,6 +28,29 @@ $('#hide').on('click', function() {
   $('.non').show();
 });*/
 
+$('.product').on('click', function() {
+  var balls = $(this);
+  console.log(products[0].text);
+  /*var pop = $('#popup');
+  pop.css('display', 'block');
+  pop.html(balls);*/
+
+});
+
+var $grid = $('.grid');
+$(window).on('load', function(){
+  $grid.append(
+    '<div class="product grid-2">\
+      <div class= "image">\
+      <img src=' + products[0].img + '> \
+      </div> \
+    </div>');
+
+  console.log(products[0].name);
+
+});
+
+
 var $grid = $('.grid'),
   fitWidthTorF = true,
   winWidth = $(window).width();
@@ -37,7 +60,7 @@ $(window).resize(function(){
 });
 
 
-if(winWidth < 630) {
+if(winWidth < 500) {
   fitWidthTorF = false;
 }
 
@@ -56,9 +79,14 @@ if(winWidth < 630) {
     }
 
   });
-
+/*$grid.on( 'click', '.product', function() {
+  // change size of item by toggling gigante class
+  $( this ).toggleClass('gigante');
+  $grid.isotope('layout');
+});*/
 
 }); // end main function
+
 /*if(width > 630) {
   console.log(masonry.gutter);
   }*/
---
+++ js/products.js
@@ -0,0 +1,72 @@
+var products = [
+	{
+		'name': 'Half Dollar',
+		'class': 'product non', 
+		'img': 'img/homepage/JFK-Half-Dollar-Coin-Ring-6.jpg',
+		'text': 'Lorem ipsum dolor sit amet, \
+			 consectetur adipiscing elit. Sed feugiat consectetur pellentesque.\
+			 Nam ac elit risus, ac blandit dui. Duis rutrum porta tortor ut convallis.\
+			 Duis rutrum porta tortor ut convallis.',
+		'price': 50
+	},
+	{
+		'name': 'Ike Dollar',
+		'class': 'product grid-2 non', 
+		'img': 'img/homepage/Eisenhower-Silver-Dollar-Coin-Ring-3.jpg',
+		'text': 'Lorem ipsum dolor sit amet, \
+			 consectetur adipiscing elit. Sed feugiat consectetur pellentesque.\
+			 Nam ac elit risus, ac blandit dui. Duis rutrum porta tortor ut convallis.\
+			 Duis rutrum porta tortor ut convallis.',
+		'price': 10
+	},
+	{
+		'name': 'Franklin Half Dollar',
+		'class': 'product', 
+		'img': 'img/homepage/Ben-Franklin-Half-Dollar-Coin-Ring-6.jpg',
+		'text': 'Lorem ipsum dolor sit amet, \
+			 consectetur adipiscing elit. Sed feugiat consectetur pellentesque.\
+			 Nam ac elit risus, ac blandit dui. Duis rutrum porta tortor ut convallis.\
+			 Duis rutrum porta tortor ut convallis.',
+		'price': 10
+	},
+	{
+		'name': 'Coin Ring',
+		'class': 'product non', 
+		'img': 'img/homepage/Coin-ring-2-16.jpg',
+		'text': 'Lorem ipsum dolor sit amet, \
+			 consectetur adipiscing elit. Sed feugiat consectetur pellentesque.\
+			 Nam ac elit risus, ac blandit dui. Duis rutrum porta tortor ut convallis.\
+			 Duis rutrum porta tortor ut convallis.',
+		'price': 40
+	},
+	{
+		'name': 'Half Dollar',
+		'class': 'product non', 
+		'img': 'img/homepage/Dime-Coin-Ring-2.jpg',
+		'text': 'Lorem ipsum dolor sit amet, \
+			 consectetur adipiscing elit. Sed feugiat consectetur pellentesque.\
+			 Nam ac elit risus, ac blandit dui. Duis rutrum porta tortor ut convallis.\
+			 Duis rutrum porta tortor ut convallis.',
+		'price': 50
+	},
+	{
+		'name': 'Half Dollar',
+		'class': 'product non', 
+		'img': 'img/homepage/Morgan-Dollar-Coin-Ring-4.jpg',
+		'text': 'Lorem ipsum dolor sit amet, \
+			 consectetur adipiscing elit. Sed feugiat consectetur pellentesque.\
+			 Nam ac elit risus, ac blandit dui. Duis rutrum porta tortor ut convallis.\
+			 Duis rutrum porta tortor ut convallis.',
+		'price': 50
+	},
+	{
+		'name': 'Half Dollar',
+		'class': 'product non', 
+		'img': 'img/homepage/State-quarter-rings-3.jpg',
+		'text': 'Lorem ipsum dolor sit amet, \
+			 consectetur adipiscing elit. Sed feugiat consectetur pellentesque.\
+			 Nam ac elit risus, ac blandit dui. Duis rutrum porta tortor ut convallis.\
+			 Duis rutrum porta tortor ut convallis.',
+		'price': 50
+	}
+];
